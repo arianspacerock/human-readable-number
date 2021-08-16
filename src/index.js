f47e11789d64e83fdc2e9fdf741420354308de99
arianspacerock <siropemptyspace@gmail.com>
2021-08-16 23:29:58 +0300

    complete HRN task

--- src/index.js
+++ src/index.js
@@ -1,62 +1,76 @@
-module.exports = function toReadable (number) {
-    const newObj = {  
-        0: 'zero',
-         1: 'one',
-         2: 'two',
-         3: 'three',
-         4: 'four',
-         5: 'five',
-         6: 'six',
-         7: 'seven',
-         8: 'eight',
-         9: 'nine'
-       } 
-       
-       
-       const newObjTwo = {  
-        10: 'ten',
-         11: 'eleven',
-         12: 'twelve',
-         13: 'thirteen',
-         14: 'fourteen',
-         15: 'fifteen',
-         16: 'sixteen',
-         17: 'seventeen',
-         18: 'eighteen',
-         19: 'nineteen',
-         20: "twenty",
-         30: "thirty",
-         40: "forty",
-         50: "fifty",
-         60: "sixty",
-         70: "seventy",
-         80: "eighty",
-         90: "ninety",
-     
-       } 
-       
-       const newNumberString = String(number).split('') 
-       
-       for(let i = 0; i < newNumberString.length; i++) {   
-        
-       if(newNumberString.length === 1) {
-       
-           //const test = `${newNumberString[0]}0`
-           
-           const test2 = `${newNumberString[0]}`
-           
-           return `${newObj[test2]}`
-           //`${newObjTwo[test]} hundred ${newObj[test2]}`
-           } 
-       /*if(newNumberString.length === 2) {
-       
-           const test = `${newNumberString[20]}`
-           
-           const test2 = `${newNumberString[2]}`
-           
-           return `${newObj[test2]} ${newObjTwo[test]}`    
-        */  
-       }
-     }
-     console.log(toReadable(1))
 
+module.exports = function toReadable (number) {
+  const newObj={
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4:"four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12:"twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+  }
+
+  let str=""; 
+  let one=0;
+  let ten=0;
+  let hundred=0;
+  if (number===100){
+    str+="one hundred";
+  }
+  else if ((number>=0)&&(number<=19)){
+    str+=newObj[number];
+  }
+  else if ((number>=20)&&(number<=99)){
+    if (number%10===0){
+      str+=newObj[number];
+    }
+    else {
+      one=number%10;
+      ten=(Math.floor(number/10))*10;
+      str+=newObj[ten]+ " "+ newObj[one];
+      }
+  }
+  else if ((number>=101)&&(number<=999)){
+    if (number%100===0){
+      str+=newObj[number/100]+" hundred";
+    }
+    else {
+      hundred=Math.floor(number/100);
+      ten=number-(hundred*100);
+       if ((ten>0)&&(ten<20)){
+         str+=newObj[hundred]+ " hundred "+ newObj[ten];
+       }
+       else if (ten%10===0){
+         str+=newObj[hundred]+ " hundred "+ newObj[ten];
+       }
+       else {
+         one=ten%10;
+         ten=ten-one;
+         str+=newObj[hundred]+ " hundred "+ newObj[ten]+" "+newObj[one];
+       }
+    }
+  }
+
+    return str.trim();
+  }
